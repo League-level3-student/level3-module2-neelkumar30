450ab0ea7122a07a7452b69289fa6dbef87bf128
league <league@wts-imac-11.attlocal.net>
2019-02-02 17:25:45 -0800

    Module 2 (IP)

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -3,6 +3,39 @@ package _02_More_Algorithms;
 import java.util.List;
 
 public class Algorithms {
+	public static String multiply(int a, int b) {
+		int temp = a *b;
+		return a + " x " + b + " = " + temp;
+	}
+	public static boolean isPrime(int a) {
+		int root = (int) Math.sqrt(a);
+		for (int i = 2; i < root+1; i++) {
+			int rem = a%i;
+			if(rem == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	public static boolean isSquare(int a) {
+		double root =Math.sqrt(a);
+		if(root == (int) root) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public static boolean isCube(int a) {
+		double root =Math.cbrt(a);
+		if(root == (int) root) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 	public static int findBrokenEgg(List<String> eggs) {
 		return 0;
 	}
--- src/_02_More_Algorithms/_00_MoreAndMoreTests.java
+++ src/_02_More_Algorithms/_00_MoreAndMoreTests.java
@@ -9,53 +9,54 @@ import org.junit.Test;
  * **/
 
 public class _00_MoreAndMoreTests {
+	Algorithms run = new Algorithms();
+	
+	@Test
+	  public void MultTest() {
 
-//	@Test
-//	  public void MultTest() {
-//
-//	    assertEquals("10 x 0 = 0", multiply(10, 0));
-//	    assertEquals("10 x 10 = 100", multiply(10, 10));
-//	    assertEquals("8 x 11 = 88", multiply(8, 11));
-//	  }
-//
-//	@Test
-//	  public void PrimeTest() {
-//
-//	    assertTrue(isPrime(3));
-//	    assertTrue(isPrime(5));
-//	    assertTrue(isPrime(541));
-//	    assertFalse(isPrime(4));
-//	    assertFalse(isPrime(12));
-//	    assertFalse(isPrime(527));
-//
-//	  }
-//
-//	@Test
-//	  public void SquareTest() {
-//
-//	    assertTrue(isSquare(4));
-//	    assertTrue(isSquare(144));
-//	    assertTrue(isSquare(64));
-//	    assertTrue(isSquare(10201));
-//	    assertTrue(isSquare(1));
-//	    assertFalse(isSquare(3));
-//	    assertFalse(isSquare(22));
-//	    assertFalse(isSquare(143));
-//
-//	  }
-//
-//	@Test
-//	  public void CubeTest() {
-//
-//	    assertTrue(isCube(27));
-//	    assertTrue(isCube(216));
-//	    assertTrue(isCube(729));
-//	    assertTrue(isCube(1));
-//	    assertFalse(isCube(3));
-//	    assertFalse(isCube(22));
-//	    assertFalse(isCube(143));
-//
-//	  }
+	    assertEquals("10 x 0 = 0", run.multiply(10, 0));
+	    assertEquals("10 x 10 = 100", run.multiply(10, 10));
+	    assertEquals("8 x 11 = 88", run.multiply(8, 11));
+	  }
+
+	@Test
+	  public void PrimeTest() {
+
+	    assertTrue(run.isPrime(3));
+	    assertTrue(run.isPrime(5));
+	    assertTrue(run.isPrime(541));
+	    assertFalse(run.isPrime(4));
+	    assertFalse(run.isPrime(12));
+	    assertFalse(run.isPrime(527));
+
+	  }
+
+	@Test
+	  public void SquareTest() {
+
+	    assertTrue(run.isSquare(4));
+	    assertTrue(run.isSquare(144));
+	    assertTrue(run.isSquare(64));
+	    assertTrue(run.isSquare(10201));
+	    assertTrue(run.isSquare(1));
+	    assertFalse(run.isSquare(3));
+	    assertFalse(run.isSquare(22));
+	    assertFalse(run.isSquare(143));
+
+	  }
+
+	@Test
+	  public void CubeTest() {
+
+	    assertTrue(run.isCube(27));
+	    assertTrue(run.isCube(216));
+	    assertTrue(run.isCube(729));
+	    assertTrue(run.isCube(1));
+	    assertFalse(run.isCube(3));
+	    assertFalse(run.isCube(22));
+	    assertFalse(run.isCube(143));
+
+	  }
 
 
 
